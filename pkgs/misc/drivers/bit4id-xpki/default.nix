ae3bac9d8c050dcfad1ed4074f89c9825b96f34d
spookyh83 <133262116+spookyh83@users.noreply.github.com>
2023-05-12 10:48:01 +0200

    Update default.nix
    added license and platforms

--- pkgs/misc/drivers/bit4id-xpki/default.nix
+++ pkgs/misc/drivers/bit4id-xpki/default.nix
@@ -1,25 +1,19 @@
-{ stdenv, fetchurl, dpkg }:
-
+{ lib, stdenv, fetchurl, dpkg, pcsclite, opensc, pcsctools, ccid }:
 stdenv.mkDerivation rec {
-  name = "libbit4xpki-idemia";
-
+  name = "bit4xpki";
   src = fetchurl {
     url = "https://swdownload1.agenziaentrate.gov.it/pub/sanita/libbit4xpki-idemia-amd64.1.4.10-622.deb";
     sha256 = "1mq6xng97d5s94h84p1clsnh56g245xcr1pbjm3ryjf43w7jlqlx";
   };
-
   unpackPhase = "dpkg-deb -x $src $out";
-
-  meta = {
+  meta = with lib; {
     description = "Libraries for the XPKI client used by the Italian CNS.";
-    homepage = "https://swdownload1.agenziaentrate.gov.it";
-    #license = stdenv.lib.licenses.unfree;
+    homepage = "https://sistemats1.sanita.finanze.it/portale/elenco-driver-cittadini-modalita-accesso";
+    platforms = [ "x86_64-linux" ];
+    license = licenses.unfree;
   };
     buildInputs = [ dpkg ];
-
-  # set dpkg PATH
-  shellHook = ''
+    shellHook = ''
     export PATH="${dpkg}/bin:$PATH"
   '';
-
 }
